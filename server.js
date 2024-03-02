3734aac426072722dbc4cf411e54098f8e69bc28
Tutik Maryana <m1@M1s-MacBook-Air.local>
2024-03-02 21:17:53 +0700

    setting routes di server js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app=express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+//panggil route
+var route  = require('./route');
+route(app);
+
 app.listen(3000, () => {
     console.log(`Server started on port`);
 });
